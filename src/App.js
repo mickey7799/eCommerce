4f44df02f215365445a082d1f60db74a05b1b2cb
mickey7799 <vickeylee.w@gmail.com>
2021-02-24 22:57:39 +1100

    Added Shop page data and structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
